e802c6fc15429c1050ef0f6187fbbe602e0add4b
Gianmauro Cuccuru <gmauro@crs4.it>
2018-01-29 15:12:56 +0100

    first step to vcfminer

---
+++ rules/scripts/format_annotation.py
@@ -0,0 +1,17 @@
+
+with open(snakemake.input, 'r') as fp:
+    header = fp.readline()
+    body = fp.readlines()
+
+header = header.translate({ord('\t'): ','})
+strings_to_replace = (('Chromosome', 'CHROM'), ('StartPositionHg19', 'POS'))
+for s in strings_to_replace:
+    header = header.replace(s[0], s[1], 1)
+
+vcf = ''.join(map(lambda x: ''.join('chr' + x), body))
+chars_to_replace = {ord('='): '|', ord(';'): '|', ord(' '): '|'}
+vcf = vcf.translate(chars_to_replace)
+
+with open(snakemake.output, 'w') as fp:
+    fp.write(header)
+    fp.write(vcf)
