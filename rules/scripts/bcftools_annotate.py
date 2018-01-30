8281d2ae729953c5458f3f87bdd73c05966354cd
Gianmauro Cuccuru <gmauro@crs4.it>
2018-01-30 15:57:26 +0100

    annotation4vcfminer

--- rules/scripts/bcftools_annotate.py
+++ rules/scripts/bcftools_annotate.py
@@ -1,12 +1,19 @@
 from subprocess import run
 
-with open(snakemake.input['cname'], 'r') as fp:
-        cname = fp.read()
+if snakemake.params['cmd'] == 'add':
+    with open(snakemake.input['cname'], 'r') as fp:
+            cname = fp.read()
 
-run(['bcftools', 'annotate',
-     '-a', snakemake.input['gz'],
-     '-h', snakemake.params.header,
-     '-c', cname,
-     '-o', snakemake.output[0],
-     snakemake.input['vcf']
-     ])
+    run(['bcftools', 'annotate',
+         '-a', snakemake.input['gz'],
+         '-h', snakemake.params.header,
+         '-c', cname,
+         '-o', snakemake.output[0],
+         snakemake.input['vcf']
+         ])
+elif snakemake.params['cmd'] == 'remove':
+    run(['bcftools', 'annotate',
+         snakemake.params['params'],
+         '-o', snakemake.output[0],
+         snakemake.input[0]
+         ])
--- rules/scripts/bcftools_annotate_remove.py
+++
@@ -1,7 +0,0 @@
-from subprocess import run
-
-run(['bcftools', 'annotate',
-     snakemake.params[0],
-     '-o', snakemake.output[0],
-     snakemake.input[0]
-     ])
